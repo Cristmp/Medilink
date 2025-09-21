909fefb6ea2114a1a3b0e4a3d56b76a5f8e6457c
Cristian <cristianzapata2007@gmail.com>
2025-09-21 04:09:27 -0600

    configuración calendario, composición eventos

--- app.js
+++ app.js
@@ -56,6 +56,58 @@ prevbtn.addEventListener('click', () => {
     setTimeout(updateButtons, 100);
 })
 // Tercera parte funcional de la página, "Calendario de eventos"
+const currentDate = document.querySelector('.fecha')
+const diaTag = document.querySelector('.dia')
+const prevNextIcon = document.querySelectorAll(".icons span")
+
+let date = new Date();
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+const month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+              "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+const renderCalendar = () => {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
+    LastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--){
+        liTag += `<li class="inactive">${LastDateofLastMonth - i + 1}</li>`
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++){
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+                     && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++){
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    }
+
+    currentDate.innerHTML = `${month[currMonth]} ${currYear}`;
+    diaTag.innerHTML = liTag;
+}
+renderCalendar();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        currMonth = icon.id === "prev" ?  currMonth - 1 : currMonth + 1;
+        if(currMonth < 0 || currMonth > 11) {
+            date = new Date(currYear, currMonth, new Date().getDate());
+            currYear = date.getFullYear(); 
+            currMonth = date.getMonth();
+        } else {
+            date = new Date();
+        }
+
+
+        renderCalendar();
+    })
+})
 
 // Cuarta parte funcional de la página, "Busqueda de Centros de salud"
 
